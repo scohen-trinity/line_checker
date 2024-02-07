ed777ecd224e642d18386e02562674d591e47469
scohen-trinity <scohen@trinity.edu>
2024-02-07 10:42:51 -0600

    same commmit as before I just forgot to add some typing

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,17 @@ fn main() {
 
     let file_path: &str = &env_args[1];
 
-    let imported: String = import_file(file_path);
+    let line_count: usize = check_file(file_path);
+
+    println!("{} lines in file {}", line_count, file_path);
+}
+
+fn check_file(file_path: &str) -> usize {
+    let imported: String = fs::read_to_string(file_path).expect("Could not read file.");
     
     let lines: Vec<&str> = imported.lines().filter(|&line| !line.starts_with("//") && !line.trim().is_empty()).collect();
 
     let line_count: usize = lines.len();
 
-    println!("{} lines in file {}", line_count, file_path);
-}
-
-fn import_file(file_path: &str) -> String {
-
-    let read_file: String = fs::read_to_string(file_path).expect("Could not read file");
-
-    read_file
+    line_count
 }
